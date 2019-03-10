937500876a2264c943d85a4e10e85b2392f3a954
Marcelo <mtmn@VirusLabPro.local>
2019-03-10 17:00:57 -0300

    Adding two way binding to textbox

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -13,6 +13,7 @@ export class ProductListComponent {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
+    listFilter: string = 'cart';
     products: IProduct[] = 
     [
         {
