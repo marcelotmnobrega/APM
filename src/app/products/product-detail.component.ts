ed81d3b671bd8baa1a7367c26b9d97f0a62c00ff
Marcelo Nobrega <marcelotmnobrega@gmail.com>
2019-04-14 16:18:44 -0300

    Adding back button and guard to validate product id.

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-
+import { ActivatedRoute } from "@angular/router";
+import { Router } from "@angular/router";
 import { IProduct } from "./product";
 
 @Component({
@@ -11,7 +12,26 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = "Product Detail";
   product: IProduct;
 
-  constructor() {}
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    let id = +this.activatedRoute.snapshot.paramMap.get("id");
+    // the + is used to transform from str to int
+    this.pageTitle += `: ${id}`;
+    this.product = {
+      productId: id,
+      productName: "Leaf Rake",
+      productCode: "GSNG-2343",
+      releaseDate: "Dec 22, 2020",
+      description: "Leaf rake with 48-inch wooden handle",
+      price: 19.95,
+      starRating: 3.2,
+      imageUrl:
+        "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+    };
+  }
+
+  onBack(): void {
+    this.router.navigate(["/products"]);
+  }
 }
